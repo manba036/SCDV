356950f2bd1e54274aff2267de61aa31e0af7081
Manba Osamu <manba@zf7.so-net.ne.jp>
2019-08-16 14:39:22 +0900

    Add Dockerfile and update livedoor-new-corpus-test.ipynb

---
+++ run_jupyter.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+wget https://www.rondhuit.com/download/ldcc-20140209.tar.gz
+tar xvfz ldcc-20140209.tar.gz -C ./japanese-dataset/livedoor-news-corpus
+rm -f ldcc-20140209.tar.gz
+cp -rf ./japanese-dataset/livedoor-news-corpus/text/* ./japanese-dataset/livedoor-news-corpus/
+rm -Rf ./japanese-dataset/livedoor-news-corpus/text
+
+docker build -t scdv ./docker_scdv
+docker run --rm -it --name=scdv -p 8888:8888 -v `pwd`:/work/SCDV scdv
